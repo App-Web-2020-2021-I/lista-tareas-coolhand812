6b9c18d9c83a0e696c75c76b93c20e2f1f697d6f
Joe Diaz <Coolhand812@alumnos.uaslp.edu.mx>
2020-10-12 21:04:42 -0500

    added lista tareas id & fin JS code

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+function agregarTarea(datos){
+    let html = `<li class="list-group-item">
+    <div class="row">
+        <div class="col-1 d-flex">
+            <input type="checkbox" class="form-control align-self-center">
+        </div>
+        <div class="col-10 row m-0 p-0">
+            <div class="col-9 h5">
+                $(datos.titulo)
+            </div>
+            <div class="col-3 text-right">
+               <i>$(datos.fecha)</i>
+            </div>
+            <div class="col-12 text-justify">
+                $(datos.descripcion)
+            </div>
+        </div>
+        <div class="col-1">
+            <button class="btn btn-danger">&times;</button>
+        </div>
+    </div>
+</li>`;
+}
+
 $(document).ready(function(){
-    
+    $("#form-tarea").submit(function(e){
+        e.preventDefault();
+
+        let titulo = $("#titulo").val();
+        let fecha = $("#fecha").val();
+        let descripcion = $("#descripcion").val();
+
+        let datos = {
+            titulo,
+            fecha,
+            descripcion
+        }
+
+        agregarTarea(datos);
+    });
 });
