9536e648936c477a406c309f21297a82eb1c76de
Joe Diaz <Coolhand812@alumnos.uaslp.edu.mx>
2020-10-12 21:12:51 -0500

    fixed some of the JS

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 function agregarTarea(datos){
-    let html = `<li class="list-group-item">
-    <div class="row">
-        <div class="col-1 d-flex">
-            <input type="checkbox" class="form-control align-self-center">
-        </div>
-        <div class="col-10 row m-0 p-0">
-            <div class="col-9 h5">
-                $(datos.titulo)
+    let html = 
+    `<li class="list-group-item">
+        <div class="row">
+            <div class="col-1 d-flex">
+                <input type="checkbox" class="form-control align-self-center">
             </div>
-            <div class="col-3 text-right">
-               <i>$(datos.fecha)</i>
+            <div class="col-10 row m-0 p-0">
+                <div class="col-9 h5">
+                    $(datos.titulo)
+                </div>
+                <div class="col-3 text-right">
+                    <i>$(datos.fecha)</i>
+                </div>
+                <div class="col-12 text-justify">
+                    $(datos.descripcion)
+                </div>
             </div>
-            <div class="col-12 text-justify">
-                $(datos.descripcion)
+            <div class="col-1">
+                <button class="btn btn-danger">&times;</button>
             </div>
         </div>
-        <div class="col-1">
-            <button class="btn btn-danger">&times;</button>
-        </div>
-    </div>
-</li>`;
+    </li>`;
+
+    $("#lista-tareas")
 }
 
 $(document).ready(function(){
